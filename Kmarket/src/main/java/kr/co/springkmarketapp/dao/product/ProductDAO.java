e4f9432a702a1e7abcc283d255ecd3f13334a988
Jyung01 <wldnd9895@naver.com>
2026-07-06 12:19:27 +0900

    [feat] 인덱스 상품 완성

--- Kmarket/src/main/java/kr/co/springkmarketapp/dao/product/ProductDAO.java
+++ Kmarket/src/main/java/kr/co/springkmarketapp/dao/product/ProductDAO.java
@@ -1,6 +1,7 @@
 package kr.co.springkmarketapp.dao.product;
 
 import kr.co.springkmarketapp.dto.product.ProductDTO;
+import kr.co.springkmarketapp.dto.product.ProductMainDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -12,4 +13,12 @@ public interface ProductDAO {
     List<ProductDTO> selectProductList();
     int updateProduct(ProductDTO productDTO);
     int deleteProduct(Integer productNo);
+
+    // index 상품 리스트 mapper 요청
+    List<ProductMainDTO> selectBestProducts();
+    List<ProductMainDTO> selectHitProducts();
+    List<ProductMainDTO> selectRecommendProducts();
+    List<ProductMainDTO> selectNewProducts();
+    List<ProductMainDTO> selectPopularProducts();
+    List<ProductMainDTO> selectDiscountProducts();
 }
--- Kmarket/src/main/java/kr/co/springkmarketapp/dto/product/CategoryDTO.java
+++ Kmarket/src/main/java/kr/co/springkmarketapp/dto/product/CategoryDTO.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -16,4 +19,8 @@ public class CategoryDTO {
     private Integer depth;
     private Integer sortOrder;
     private String useYn;
+
+    // 화면에서 1차 카테고리 아래 2차 카테고리 출력용
+    private List<CategoryDTO> children = new ArrayList<>();
+
 }
