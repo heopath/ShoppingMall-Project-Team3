31a0fe3a8ff86473736d75fb54b3b5c23dcf099e
sub1n17 <sub1n17@naver.com>
2026-07-09 17:28:23 +0900

    feat: 마이페이지 구현

--- Kmarket/src/main/java/kr/co/springkmarketapp/dao/my/MyDAO.java
+++ Kmarket/src/main/java/kr/co/springkmarketapp/dao/my/MyDAO.java
@@ -1,6 +1,8 @@
 package kr.co.springkmarketapp.dao.my;
 
+import kr.co.springkmarketapp.dto.coupon.CouponIssueDTO;
 import kr.co.springkmarketapp.dto.cs.QnaDTO;
+import kr.co.springkmarketapp.dto.member.MemberDTO;
 import kr.co.springkmarketapp.dto.member.SellerProfileDTO;
 import kr.co.springkmarketapp.dto.my.MemberPointDTO;
 import kr.co.springkmarketapp.dto.order.OrderItemDTO;
@@ -39,17 +41,43 @@ public interface MyDAO {
     // review_image 등록
     void insertReviewImage(ReviewImageDTO dto);
 
-    // 포인트 적립 내역 조회
+    // 메인 - 포인트 적립 내역 조회
     List<MemberPointDTO> selectPointList(Integer memberNo);
 
-    // 상품평 조회
+    // 메뉴 - 포인트 적립 내역 조회
+    List<MemberPointDTO> selectPointListWithPaging(
+            @Param("memberNo") Integer memberNo,
+            @Param("start") int start,
+            @Param("size") int size
+    );
+    // 메뉴 - 포인트 적립 내역 갯수
+    Integer selectPointCount(Integer memberNo);
+
+    // 메인 - 상품평 조회
     List<ProductReviewDTO> selectReviewList(Integer memberNo);
+    // 메뉴 - 상품평 조회
+    List<ProductReviewDTO> selectReviewListWithPaging(
+            @Param("memberNo") Integer memberNo,
+            @Param("start") int start,
+            @Param("size") int size
+    );
+    // 메뉴 -  상품평 개수
+    Integer selectReviewCount(Integer memberNo);
 
-    // 문의내역 조회
+    // 메인 - 문의내역 조회
     List<QnaDTO> selectMyQnaList(Integer memberNo);
-
+   // 메뉴 - 문의내역 조회
+    List<QnaDTO> selectMyQnaListWithPaging(
+            @Param("memberNo") Integer memberNo,
+            @Param("start") int start,
+            @Param("size") int size
+    );
     // 주문내역 갯수 조회
     Integer selectOrderCount(Integer memberNo);
+    Integer selectOrderCount(
+            @Param("memberNo") Integer memberNo,
+            @Param("startDate") String startDate,
+            @Param("endDate") String endDate);
 
     // 쿠폰 갯수 조회
     Integer selectCouponCount(Integer memberNo);
@@ -60,9 +88,29 @@ public interface MyDAO {
     // 문의내역 갯수 조회
     Integer selectQnaCount(Integer memberNo);
 
-    // 메뉴1 - 주문내역 전체조회
+    // 메뉴 - 주문내역 전체조회
     List<OrderItemDTO> selectRecentOrderList(
             @Param("memberNo") Integer memberNo,
             @Param("start") Integer start,
             @Param("size") Integer size);
+
+    List<OrderItemDTO> selectOrderPage(
+            @Param("memberNo") Integer memberNo,
+            @Param("startDate") String startDate,
+            @Param("endDate") String endDate,
+            @Param("start") Integer start,
+            @Param("size") Integer size);
+
+    // 쿠폰 내역 조회
+    List<CouponIssueDTO> selectCouponList(
+            @Param("memberNo") Integer memberNo,
+            @Param("start") int start,
+            @Param("size") int size
+    );
+
+    // 회원정보 조회
+    MemberDTO selectMemberSetting(Integer memberNo);
+
+    // 회원정보 수정
+    void updateMemberSetting(MemberDTO dto);
 }
--- Kmarket/src/main/java/kr/co/springkmarketapp/dto/member/MemberDTO.java
+++ Kmarket/src/main/java/kr/co/springkmarketapp/dto/member/MemberDTO.java
@@ -50,4 +50,25 @@ public class MemberDTO {
                 .addr2(addr2)
                 .build();
     }
+
+    // 마이페이지 나의 설정 - 아이디 마스킹 처리
+    public String getMaskedMemberId(){
+
+        if(memberId == null) return "";
+
+        return memberId.substring(0,3)
+                + "*".repeat(memberId.length()-3);
+    }
+
+    // 마이페이지 나의 설정 - 이름 마스킹 처리
+    public String getMaskedName(){
+
+        if(name == null || name.length() < 2){
+            return name;
+        }
+
+        return name.substring(0,1)
+                + "*".repeat(name.length()-2)
+                + name.substring(name.length()-1);
+    }
 }
