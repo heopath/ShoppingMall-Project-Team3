b7e4a0b12cf232a6fe6b339cacd277c3afb3aa3a
inroad <soo_os@naver.com>
2026-07-13 12:39:29 +0900

    정책약관 수정

--- Kmarket/src/main/java/kr/co/springkmarketapp/controller/policy/PolicyController.java
+++ Kmarket/src/main/java/kr/co/springkmarketapp/controller/policy/PolicyController.java
@@ -1,33 +1,53 @@
 package kr.co.springkmarketapp.controller.policy;
 
+import kr.co.springkmarketapp.service.product.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class PolicyController {
 
+    private final CategoryService categoryService;
+
+    private void addCategoryModel(Model model) {
+        model.addAttribute("categoryTree", categoryService.getCategoryTree());
+    }
+
+    @GetMapping("/policy")
+    public String index() {
+        return "redirect:/policy/buyer";
+    }
+
     @GetMapping("/policy/buyer")
-    public String buyer() {
+    public String buyer(Model model) {
+        addCategoryModel(model);
         return "policy/buyer";
     }
 
     @GetMapping("/policy/finance")
-    public String finance() {
+    public String finance(Model model) {
+        addCategoryModel(model);
         return "policy/finance";
     }
 
     @GetMapping("/policy/location")
-    public String location() {
+    public String location(Model model) {
+        addCategoryModel(model);
         return "policy/location";
     }
 
     @GetMapping("/policy/privacy")
-    public String privacy() {
+    public String privacy(Model model) {
+        addCategoryModel(model);
         return "policy/privacy";
     }
 
     @GetMapping("/policy/seller")
-    public String seller() {
+    public String seller(Model model) {
+        addCategoryModel(model);
         return "policy/seller";
     }
 }
